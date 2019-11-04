9b33924b5c0bb4fbceaa8c805b9107379713dff1
Pasha Shalunov <shalunov.pasha@gmail.com>
2019-11-04 22:44:25 +0400

    move function to app.js

--- app.rb
+++ app.rb
@@ -12,6 +12,28 @@ class Barber < ActiveRecord::Base
 end
 
 get '/' do
+	@barbers = Barber.all
   erb :index
 end
 
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barder = params[:barber]
+	@color = params[:color]
+
+	c = Client.new
+	c.name = @username
+	c.phone = @phone
+	c.datestamp = @datetime
+	c.barber = @barber
+	c.color = @color
+	c.save
+
+	erb "<h2>Спасибо, вы записались.</h2>"
+end
